c709e76e270e133b48648e112d2feb7b82beefea
ilya64839 <135427851+ilya64839@users.noreply.github.com>
2026-06-26 13:49:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
-const CACHE = 'liza-fitness-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'liza-fitness-v2';
+const ASSETS = ['/manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
